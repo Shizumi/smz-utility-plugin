098d2c2e524ffebb7fc2c5598cbee9c7d3fddf67
Shizumi Yoshiaki <ni.muzihs@shizum.in>
2020-09-22 12:29:19 +0900

    :+1: ファイル存在チェック関数追加

--- smz-utility.php
+++ smz-utility.php
@@ -7,3 +7,20 @@
  * Author URI:  https://blog.spicadots.com/
  */
 
+if ( ! function_exists( 'image_url_exists' ) ) {
+	/**
+	 * ファイルが存在するかのチェック
+	 *
+	 * @param $url
+	 * @return bool
+	 */
+	function image_url_exists( $url ) {
+		$parsed_url = parse_url( $url )['path'];
+		$upload_dir = wp_upload_dir()[ 'basedir' ];
+		$upload_str = apply_filters( 'smz_upload_dir', 'wp-content/uploads' );
+
+		$file_path = $upload_dir . substr( $parsed_url , strpos( $parsed_url, $upload_str ) + strlen( $upload_str ) );
+
+		return ( file_exists( $file_path ) && is_file( $file_path ) && is_readable( $file_path ) );
+	}
+}
